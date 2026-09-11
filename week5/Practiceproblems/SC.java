601479b8a57cd4dcd4435d00ce125255fdb59df2
bhanu-acer <bhanu2007gupta@gmail.com>
2026-09-12 00:58:16 +0530

    Add Week 5 SC.java

---
+++ week5/Practiceproblems/SC.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class SC {
+    static void curveScores(int[] scores, int bonus) {
+        for (int i = 0; i < scores.length; i++) scores[i] += bonus;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter number of scores: ");
+        int n = sc.nextInt();
+        int[] scores = new int[n];
+        System.out.println("Enter scores:");
+        for (int i = 0; i < n; i++) scores[i] = sc.nextInt();
+        System.out.print("Enter bonus: ");
+        int bonus = sc.nextInt();
+        curveScores(scores, bonus);
+        System.out.println("Final Scores: " + Arrays.toString(scores));
+        sc.close();
+    }
+}
