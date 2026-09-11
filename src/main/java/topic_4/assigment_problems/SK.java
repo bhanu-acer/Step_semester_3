9e49647236d485866f8f258661d9cb551b33305d
bhanu-acer <bhanu2007gupta@gmail.com>
2026-09-12 00:22:46 +0530

    Add Week 4 assignment problem SK

---
+++ src/main/java/topic_4/assigment_problems/SK.java
@@ -0,0 +1,58 @@
+import java.util.*;
+
+public class SK {
+
+    static int subarraySum(int[] nums, int k) {
+
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        map.put(0, 1);
+
+        int currentSum = 0;
+        int count = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+
+            currentSum += nums[i];
+
+            int required = currentSum - k;
+
+            if (map.containsKey(required)) {
+                count += map.get(required);
+            }
+
+            map.put(
+                currentSum,
+                map.getOrDefault(currentSum, 0) + 1
+            );
+        }
+
+        return count;
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter number of elements: ");
+        int n = sc.nextInt();
+
+        int[] nums = new int[n];
+
+        System.out.println("Enter elements:");
+
+        for (int i = 0; i < n; i++) {
+            nums[i] = sc.nextInt();
+        }
+
+        System.out.print("Enter k: ");
+        int k = sc.nextInt();
+
+        System.out.println(
+            "Number of subarrays: " +
+            subarraySum(nums, k)
+        );
+
+        sc.close();
+    }
+}
